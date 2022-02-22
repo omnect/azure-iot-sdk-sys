d497c8e3477dc388035780ea179ec75b85532a4e
JanZachmann <jan.zachmann@conplement.de>
2022-02-22 07:30:16 +0100

    added device twin wrapper

--- wrapper.h
+++ wrapper.h
@@ -2,4 +2,5 @@
 #include <azureiot/iothub_client.h>
 #include <azureiot/iothub_message.h>
 #include <azureiot/iothub_module_client_ll.h>
+#include <azureiot/iothub_device_client_ll.h>
 #include <azureiot/iothubtransportmqtt.h>
