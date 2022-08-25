5e4b54fcdd7d5c6ef52758db9cc99f90292312f7
JanZachmann <jan.zachmann@conplement.de>
2022-08-25 16:33:59 +0200

    added iothub_client_version.h

--- wrapper.h
+++ wrapper.h
@@ -1,5 +1,6 @@
 #include <azureiot/iothub.h>
 #include <azureiot/iothub_client.h>
+#include <azureiot/iothub_client_version.h>
 #include <azureiot/iothub_message.h>
 #include <azureiot/iothub_module_client_ll.h>
 #include <azureiot/iothub_device_client_ll.h>
