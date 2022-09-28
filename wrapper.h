fd4df63858d65598cd543ac59a4aa3d017d26b4e
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2022-09-28 13:24:19 +0200

    added `iothub_client_options.h` to wrapper
    Signed-off-by: Marcel Lilienthal <134974+mlilien@users.noreply.github.com>

--- wrapper.h
+++ wrapper.h
@@ -1,5 +1,6 @@
 #include <azureiot/iothub.h>
 #include <azureiot/iothub_client.h>
+#include <azureiot/iothub_client_options.h>
 #include <azureiot/iothub_client_version.h>
 #include <azureiot/iothub_message.h>
 #include <azureiot/iothub_module_client_ll.h>
