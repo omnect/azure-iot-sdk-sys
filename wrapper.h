545bc319f0c40a20837dca52ed35f4e9a329f498
Jan Zachmann <jan.zachmann@conplement.de>
2023-06-30 17:49:24 +0200

    switched from azure-iot-sdk-c lower level layer to convenient layer

--- wrapper.h
+++ wrapper.h
@@ -3,6 +3,6 @@
 #include <azureiot/iothub_client_options.h>
 #include <azureiot/iothub_client_version.h>
 #include <azureiot/iothub_message.h>
-#include <azureiot/iothub_module_client_ll.h>
-#include <azureiot/iothub_device_client_ll.h>
+#include <azureiot/iothub_module_client.h>
+#include <azureiot/iothub_device_client.h>
 #include <azureiot/iothubtransportmqtt.h>
