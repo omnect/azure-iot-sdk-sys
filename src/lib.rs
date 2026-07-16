82e09fcc97a5afdda4ae253b84cc93a7839b866c
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2026-07-16 08:21:33 +0200

    chore: update bindgen to 0.72.x to enable yocto wrynose support
    Signed-off-by: Marcel Lilienthal <134974+mlilien@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
+#![allow(unnecessary_transmutes)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::ptr_offset_with_cast)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
